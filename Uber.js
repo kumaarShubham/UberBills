b35a7a849aac3ef22da9c2ba5eb55163d3858b8f
kumaarShubham <118390399+kumaarShubham@users.noreply.github.com>
2022-11-17 00:45:32 +0530

    Reduces # of lines
    Made comments single-lined

--- Uber.js
+++ Uber.js
@@ -2,14 +2,14 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
 
-let receiptsSegregated = false
+let receiptsSegregated = false;
 
 /**
  * @desc Segregates all receipts into their respective date folder
  */
 function segregateReceipts() {
 
-    /* Reading folder "Receipts" containing all receipts */
+	/* Reading folder "Receipts" containing all receipts */
 	let receipts = fs.readdirSync("Receipts");
 
 	receipts.forEach( async receipt => {
@@ -19,29 +19,27 @@ function segregateReceipts() {
 
 		let dataBuffer = fs.readFileSync(receiptPath);
 
-        /* Parsing each PDF */
+		/* Parsing each PDF */
 		await pdf(dataBuffer).then(function (data) {
 			dataArr = data.text.split(/\r?\n/);
 			let date = dataArr[3].split(" ")[0];
 			date = date.replaceAll("/", "-");
 
-            /* Initialises new folder "Invoices" which shall contain
-               dated folders */
+			/* Initialises new folder "Invoices" which shall contain dated folders */
 			let datedFolderPath = "./Invoices/" + date;
 
-            /* Checks if a folder with path datedFolderPath exists,
-               if not, it will create one */
+			/* Checks if a folder with path datedFolderPath exists, if not, it will create one */
 			if (! fs.existsSync(datedFolderPath)) 
 				fs.mkdirSync(datedFolderPath);
 
-            /* Moves receipts into their respective dated folders */
+			/* Moves receipts into their respective dated folders */
 			fs.rename(receiptPath, datedFolderPath + "/" + receipt, function (err) {
 				if (err) 
 					throw err;
 			});
 		});
 	});
-    receiptsSegregated = true
+	receiptsSegregated = true;
 }
 
 /**
@@ -49,20 +47,20 @@ function segregateReceipts() {
  */
 async function calculate_PerDay_TotalFare() {
 
-    /* Reading "Invoices" folder */
+	/* Reading "Invoices" folder */
 	let folders = fs.readdirSync("./Invoices");
 
 	for (let i = 0; i < folders.length; i++) {
 		let folder = folders[i];
 
-        /* Reading a dated folder */
+		/* Reading a dated folder */
 		let receipts = fs.readdirSync("./Invoices/" + folder);
 		let totalFare = 0;
 
-        /* Looping in a dated folder to go through all the receipts for that date */
+		/* Looping in a dated folder to go through all the receipts for that date */
 		for (let i = 0; i < receipts.length; i++) {
 
-            /* Reading a receipt */
+			/* Reading a receipt */
 			let dataBuffer = fs.readFileSync("./Invoices/" + folder + "/" + receipts[i]);
 
 			await pdf(dataBuffer).then(function (data) {
@@ -71,13 +69,13 @@ async function calculate_PerDay_TotalFare() {
 			});
 		}
 
-        /* Printing a day's total fare (onward + outward journey) */
+		/* Printing a day's total fare (onward + outward journey) */
 		console.log(`${folder}: ${totalFare}`);
 	}
 }
 
-if(receiptsSegregated) calculate_PerDay_TotalFare()
+if(receiptsSegregated) calculate_PerDay_TotalFare();
 else{
-    segregateReceipts()
-    calculate_PerDay_TotalFare()
+	segregateReceipts();
+	calculate_PerDay_TotalFare();
 }
